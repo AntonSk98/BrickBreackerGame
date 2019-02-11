e079cd0459436d5b105aceefeaa429396197e4ab
Anton Skripin <antonskripin@gmail.com>
2019-02-11 16:58:23 +0500

    Created JFrame and the own class Gameplay which extends from the class JPanel

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,18 @@
 package com.company;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello");
-        System.out.println("ggg");
+        JFrame jFrame = new JFrame();
+        Gameplay gameplay = new Gameplay();
+        jFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        jFrame.setUndecorated(true);
+        jFrame.setTitle("Breakout ball");
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.add(gameplay);
     }
 }
